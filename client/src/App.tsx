e425db6c7b6f75405cb3fdc4eaf4b0d283597463
Yatharth <yatharth@appvintech.com>
2025-04-08 18:20:19 +0530

    adding basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import { Navigate, Route, BrowserRouter, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Dashboard } from "./pages/Dashboard";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
+import { DASHBOARD, HOME, LOGIN, SIGNUP } from "./routes/routes";
 function App() {
-  const [count, setCount] = useState(0)
+  const isAuthenticated = () => {
+    // In a real application, you would check for a token, session, etc.
+    return true;
+  };
 
+  // Private Route component to protect routes that require authentication
+  const PrivateRoute = ({ children }) => {
+    return isAuthenticated() ? children : <Navigate to={LOGIN} />;
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path={HOME} element={<Home />} />
+        <Route path={LOGIN} element={<Login />} />
+        <Route path={SIGNUP} element={<SignUp />} />
+        <Route
+          path={DASHBOARD}
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
