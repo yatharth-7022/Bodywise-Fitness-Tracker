c8df5355aa9e8d9179c2592404c9c4c7f2c7985e
Yatharth <yatharth@appvintech.com>
2025-04-14 01:03:07 +0530

    adding images

--- client/src/types/dashboard.ts
+++ client/src/types/dashboard.ts
@@ -14,3 +14,25 @@ export interface WorkoutCardProps {
   description: string;
   id: number;
 }
+export interface Exercise {
+  id: number;
+  reps: number;
+  sets: number;
+  exercise: ExerciseDetails;
+}
+export interface ExerciseDetails {
+  bodyPart: string;
+  description: string;
+  id: number;
+  imageUrl: string;
+  name: string;
+}
+export interface Routine {
+  description: string;
+  id: number;
+  imageUrl: string;
+  isDefault: boolean;
+  name: string;
+  userId: number | null;
+  exercises: Exercise[];
+}
